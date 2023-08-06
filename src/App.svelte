c82fedffd5bb4bdb96bcdaa694dd04b19ca40fd6
Won Jae Kim <86081049+Daryu-Kim@users.noreply.github.com>
2023-08-06 19:27:05 +0900

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import HomePage from "./pages/Home.svelte";
+    import AboutPage from "./pages/About/index.svelte";
+    import Router from "svelte-spa-router";
+    import NotFound from "./pages/NotFound.svelte";
+
+    export const routes = {
+        '/': HomePage,
+        '/about': AboutPage,
+        '/*': NotFound,
+    };
+</script>
+
+<main>
+    <Router {routes} />
+</main>
+
+<style></style>
